0cecc4f135c52aace4406b9d31a6ff16b0485548
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-10 14:44:29 -0300

    feat: adding another function

--- lib.cpp
+++ lib.cpp
@@ -4,3 +4,8 @@ extern "C" void showNumbers(int n1, int n2) {
     int n3 = n1 + n2;
     printf("the sum is: %d\n", n3);
 }
+
+extern "C" void subNumbers(int n1, int n2) {
+    int n3 = n1 - n2;
+    printf("the sum is: %d\n", n3);
+}
