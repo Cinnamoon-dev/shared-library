7c5c74062a1e069070fca0bd56f21b6591bad849
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-10 14:59:30 -0300

    fix: changing subtration print string

--- lib.cpp
+++ lib.cpp
@@ -7,5 +7,5 @@ extern "C" void showNumbers(int n1, int n2) {
 
 extern "C" void subNumbers(int n1, int n2) {
     int n3 = n1 - n2;
-    printf("the sum is: %d\n", n3);
+    printf("the sub is: %d\n", n3);
 }
